383df0f3e1d53a3b95bf9c4068d6c8e4b33a38e4
funti <funti@VGOLIKO>
2023-03-09 00:44:47 +0700

    Добавьте файлы проекта.

---
+++ zadachaTamagotchi/Cat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace zadachaTamagochi
+{
+    public struct Fruit
+    {
+        public int heath;
+        public string type;
+    }
+    class Cat
+    {
+        private string name;
+        private string color;
+        private int health;
+
+        public Cat(string name = "Null")
+        {
+            this.name = name;
+            health = 100;
+        }
+        public static void Meow(Cat meow)
+        {
+            Console.WriteLine("Мяу");
+        }
+        public static void Eat(Fruit fruit)
+        {
+            Console.WriteLine($"Кошка съела {fruit.type}.{fruit.heath} Здоровья");
+        }
+        public static void DoMeal(Cat a, Fruit fruit)
+        {
+            Cat.Eat(fruit);
+            a.health = a.health - fruit.heath;
+            if (a.health <= 0)
+                Console.WriteLine(" кошка погибла");
+        }
+        public static void Info(Cat a)
+        {
+            Console.WriteLine($"{a.name}, {a.color}, {a.health} HP");
+        }
+        public static void Die(Cat a)
+        {
+            --a.health;
+            Console.WriteLine(a.health);
+            if (a.health <= 0)
+                Console.WriteLine("кошка погибла");
+        }
+        public bool IsAlive()
+        {
+            bool flag = true;
+            if (health <= 0)
+            {
+                flag = false;
+            }
+            return flag;
+        }
+    }
+}
---
+++ zadachaTamagotchi/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace zadachaTamagochi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit pivo = new Fruit { type = "apple", heath = 10 };
+            Console.WriteLine("Введите имя кота");
+            Cat cat = new Cat(Console.ReadLine());
+            Cat.Info(cat);
+            while (cat.IsAlive())
+            {
+                Console.WriteLine("Меню: \n 1 - информация \n 2 - кошка помяукает \n 3 - покормить кошку \n 4 - Поест сама \n 5 - не кормить кошку \n");
+                ConsoleKey Key = Console.ReadKey().Key;
+                switch (Key)
+                {
+                    case ConsoleKey.D1:
+                        Cat.Info(cat);
+                        break;
+                    case ConsoleKey.D2:
+                        Cat.Meow(cat);
+                        break;
+                    case ConsoleKey.D3:
+                        Cat.DoMeal(cat, pivo);
+                        break;
+                    case ConsoleKey.D4:
+                        Cat.Eat(pivo);
+                        break;
+                    case ConsoleKey.D5:
+                        Cat.Die(cat);
+                        break;
+                }
+            }
+        }
+    }
+}
